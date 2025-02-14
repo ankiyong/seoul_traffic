5050ed2cff3a98e0b7578618786d3681e20941d2
aky <aky@ubuntu.myguest.virtualbox.org>
2025-02-14 14:39:12 +0900

    test: ping test

--- dags/sample.py
+++ dags/sample.py
@@ -2,16 +2,12 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
-
 from airflow import DAG
-
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
-
 from datetime import datetime
-
-# from common.get_data import api_check,get_data
 import requests,json
+
 station_id = "ST-10"
 def api_check(station_id):
     url = f"http://openapi.seoul.go.kr:8088/584a6e6b54706f703730746f44786b/json/bikeList/1/5/{station_id}" 
@@ -29,13 +25,12 @@ def get_data(station_id):
     url = f"http://openapi.seoul.go.kr:8088/584a6e6b54706f703730746f44786b/json/bikeList/1/5/{station_id}" 
     response = requests.get(url)
     data = response.json()
-    return data["rentBikeStatus"]["list_total_count"]
+    return data["rentBikeStatus"]
 
     
+
 with DAG(
-    'tutorial',
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
+    'data_pipeline',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -44,14 +39,13 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='A simple tutorial DAG',
+    description='A simple data_pipeline DAG',
     schedule_interval='1 * * * *',
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['example'],
 ) as dag:
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
     t1 = PythonOperator(
         task_id='health_check',
         python_callable=api_check,
@@ -64,6 +58,10 @@ with DAG(
         python_callable=get_data,
         op_args=[station_id]
     )
-    
+    ping_task = BashOperator(
+        task_id="ping_google",
+        bash_command="ping -c 4 google.com"
+    )
 
-    t1 >> t2
+
+    ping_task
